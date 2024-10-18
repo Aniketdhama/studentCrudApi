84e46b6e7a482465f54c62843dc4df23e43501c4
Aniketdhama <aniketdhama951@gmail.com>
2024-10-18 16:01:40 +0530

    env file

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var mongoose=require("mongoose");
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
 
-
-const DB="mongodb+srv://aniketdhama951:Basketball@cluster0.7v9wwkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const DB=`mongodb+srv://${username}:${password}@cluster0.7v9wwkf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
 mongoose.connect(DB,{
